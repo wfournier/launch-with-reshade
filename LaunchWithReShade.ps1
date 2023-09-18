9839c6e3b1cf7cbe6c4c297f94d93f9e92620723
William Fournier <33901660+wfournier@users.noreply.github.com>
2023-09-17 23:50:05 -0400

    Add files via upload

---
+++ LaunchWithReShade.ps1
@@ -0,0 +1,10 @@
+﻿$full_launch_path = $args[0]
+
+if (Test-Path $full_launch_path) {
+    $exe_name = Split-Path $full_launch_path -leaf
+
+    Start-Process -FilePath inject64.exe $exe_name
+    Start-Process -FilePath $full_launch_path
+} else {
+    Write-Output "Executable not found."
+}
